62ecb36ee2c905e34742fc6c04106014141d5fad
samiroli <samiroli789@gmail.com>
2020-01-08 10:40:58 +0545

    Updates

---
+++ precache-manifest.f39492166ceb7ae187f7e3568209913d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f7b9d30fc196671df9767fdedba369",
+    "url": "/PWA-ReactJS/index.html"
+  },
+  {
+    "revision": "9ed8ab73cfe7f7512ac8",
+    "url": "/PWA-ReactJS/static/css/main.feec53d0.chunk.css"
+  },
+  {
+    "revision": "d39d6b50ac9c26c5dafa",
+    "url": "/PWA-ReactJS/static/js/2.c5835214.chunk.js"
+  },
+  {
+    "revision": "9b177b59f5d3908ffc5ba46d5eb0523f",
+    "url": "/PWA-ReactJS/static/js/2.c5835214.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ed8ab73cfe7f7512ac8",
+    "url": "/PWA-ReactJS/static/js/main.24a1270d.chunk.js"
+  },
+  {
+    "revision": "656be24f43cd3a45912c",
+    "url": "/PWA-ReactJS/static/js/runtime-main.33ff5a32.js"
+  }
+]);

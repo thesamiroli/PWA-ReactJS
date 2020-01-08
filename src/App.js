709aa6f6afb5ce74eba9f46455ebcb95162d01e2
samiroli <samiroli789@gmail.com>
2020-01-08 12:03:14 +0545

    changed router to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Home from "./Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Detail from "./Detail";
 import { render } from "@testing-library/react";
 
